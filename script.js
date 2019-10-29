13ffb7850761e677c0939858f66f570a0425b3fb
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-10-28 19:34:12 -0500

    added an array of locations and an array of weapons

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var friends = ['Jake', 'Justin', 'Reggie', 'Dave', 'Ryan'];
 
+var locations = ['kitchen', 'bedroom', 'parlor', 'bathroom', 'basement',
+  'backyard', 'garage', 'living room', 'closet', 'driveway'];
+
+var weapons = ['knife', 'baseball bat', 'cheese grater', 'spear', 'vase',
+  'sledge hammer', 'chainsaw', 'rope', 'clothes iron', 'ice cream scoop',
+  'broad sword', 'cross bow', 'ice pick', 'axe', '2x4', 'rocket launcher',
+  'plasma sword', 'slingshot', 'katana', 'throwing star'];
+
 var accusation = 1;
 
 var createHeading = function () {
