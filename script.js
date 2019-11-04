9c00c963004780ea0f142e5fd8b907fbb1d7ed3c
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-11-03 20:03:42 -0600

    figured out code to return function correctly on click event

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-
 var friends = ['Jake', 'Justin', 'Reggie', 'Dave', 'Ryan'];
 
-var locations = ['kitchen', 'bedroom', 'parlor', 'bathroom', 'basement',
-  'backyard', 'garage', 'living room', 'closet', 'driveway'];
+  var locations = ['kitchen', 'bedroom', 'parlor', 'bathroom', 'basement',
+    'backyard', 'garage', 'living room', 'closet', 'driveway'];
 
-var weapons = ['knife', 'baseball bat', 'cheese grater', 'spear', 'vase',
-  'sledge hammer', 'chainsaw', 'rope', 'clothes iron', 'ice cream scoop',
-  'broad sword', 'cross bow', 'ice pick', 'axe', '2x4', 'rocket launcher',
-  'plasma sword', 'slingshot', 'katana', 'throwing star'];
+  var weapons = ['knife', 'baseball bat', 'cheese grater', 'spear', 'vase',
+    'sledge hammer', 'chainsaw', 'rope', 'clothes iron', 'ice cream scoop',
+    'broad sword', 'cross bow', 'ice pick', 'axe', '2x4', 'rocket launcher',
+    'plasma sword', 'slingshot', 'katana', 'throwing star'];
 
-//inserts 100 h3 headings when page loads
-var accusation = 1;
 
-var createHeading = function () {
-  var heading = $('<h3></h3>').text("Accusation " + accusation);
-  $('.col-3').append(heading);
-  accusation++;
-
-  if (accusation > 100) {
-    console.log("Reached 100");
-    return;
-  } else {
-    createHeading();
+//create 100 h3 headings on page load
+for (var i = 0; i < 101; i++) {
+  var heading = $('<h3>Accusation: ' + i + '</h3>');
+  $('body').append(heading);
+  if (i === 0) {
+    heading.hide();
   }
-}
-
-  createHeading();
-
-for ( var i = 0; i < 100; i++) {
-  $('h3').click(function() {
-    
-  });
-  
+  heading.click(clickEvent(i));
 };
 
-// 'I accuse ' + friends[i % friends.length] + ', with the ' + weapons[i % weapons.length] + ' in the ' + locations[i % locations.length] + '!'
+//this function will execute on a click event. (in the function above)
+//It brings up an alert window with an accusation sentence using objects from the arrays above
+function clickEvent(num) {
+  
+  return function () {
+    alert('I accuse ' + friends[num % friends.length] + ', with the ' + weapons[num % weapons.length] + ' in the ' + locations[num % locations.length] + '!');
+  }
+}
